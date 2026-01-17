77d19d78c392e47c2a0514e0a38fc519c7893333
emanuel <emanuel.almeida1998@outlook.com>
2026-01-16 23:25:06 -0300

    feat(log): implementação de retornos no próprio terminal sobre o usuários entrando/saindo e mensagens recebidas

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
+import logging
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 from app.manager import ConnectionManager
+
+# configuração de log
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("WebSocketServer")
 
 app = FastAPI()
 manager = ConnectionManager()
@@ -15,14 +20,21 @@ async def get():
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     # Realiza o Handshake e adiciona ao pool
+    # Ao aceitar a conexão, o FastAPI/Uvicorn envia o código 101 Switching Protocols
+    # mantendo o canal TCP aberto para comunicação bidirecional.
+    client_id = f"{websocket.client.host}:{websocket.client.port}"
+    
     await manager.connect(websocket)
+    logger.info(f"\tCliente conectado: {client_id}") # log de entrada
+
     try:
         while True:
             # Mantém a conexão aberta e escuta mensagens 
             data = await websocket.receive_text()
+            logger.info(f"\tMensagem recebida de {client_id}: {data}") # log de mensagem
             # Ao receber, faz o broadcast para todos os clientes 
-            await manager.broadcast(f"Cliente diz: {data}")
+            await manager.broadcast(f"Usuário diz: {data}")
     except WebSocketDisconnect:
         # Monitora a desconexão para limpar o pool 
         manager.disconnect(websocket)
-        await manager.broadcast("Um cliente saiu do chat.")
+        logger.info(f"\tCliente desconectado: {client_id}") # Log de saída
